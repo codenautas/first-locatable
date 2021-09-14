782a5e60b88a5b546a9e8e371bb4dcc167f91f1a
Emilio Platzer <emilioplatzer@gmail.com>
2021-09-14 16:17:35 -0300

    caso de prueba para ver que no continúa buscando

--- src/first-locatable.ts
+++ src/first-locatable.ts
@@ -1,5 +1,19 @@
-var locatePath = require("locate-path");
+// var locatePath = require("locate-path");
 
 type FirstLocatable = (pathList:string[]) => Promise<string|undefined>
 
-export const firstLocatable:FirstLocatable = locatePath
+// export const firstLocatable:FirstLocatable = locatePath
+
+import * as fs from 'fs';
+
+// /*
+export async function firstLocatable(pathList:string[]){
+    return pathList.find((path)=>fs.existsSync(path))
+};
+// */
+
+// export const firstLocatable: FirstLocatable = async ()=>"hola";
+
+// export const firstLocatable: FirstLocatable = async (pathList:string[])=>pathList.filter((path)=>fs.existsSync(path))[0];
+
+// export const firstLocatable: FirstLocatable = async (pathList:string[], opts:any)=>pathList.find((path)=>(opts?.exists || fs.existsSync)(path));
--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,12 @@ import { firstLocatable } from "../lib/first-locatable.js";
 import { strict as assert } from "assert";
 
 describe("first-locatable", function(){
+    const listOf4 = [
+        "local-testing/inexistent.js",
+        "local-testing/test.js.map",
+        "local-testing/other-inexistent.js",
+        "local-testing/test.js",
+    ];
     it("founds test.js", async function(){
         var found = await firstLocatable(["local-testing/test.js"]);
         assert.equal(found, "local-testing/test.js")
@@ -11,16 +17,25 @@ describe("first-locatable", function(){
         assert.equal(found, undefined)
     })
     it("founds test.js.map in a list", async function(){
-        var found = await firstLocatable([
-            "local-testing/inexistent.js",
-            "local-testing/test.js.map",
-            "local-testing/other-inexistent.js",
-            "local-testing/test.js",
-        ]);
+        var found = await firstLocatable(listOf4);
         assert.equal(found, "local-testing/test.js.map")
     })
     it("doesn't founds any in an empty list", async function(){
         var found = await firstLocatable([]);
         assert.equal(found, undefined)
+    })
+    it("founds test.js.map in a list and doesn't go ahead", async function(){
+        var proxy = new Proxy(listOf4,{
+            get:function(target,prop,receiver){
+                // solo tiene que buscar el 0 y el 1. 
+                if(prop=="2" || prop=="3") throw new Error("searching after found")
+                return Reflect.get(
+                    // @ts-ignore
+                    ...arguments
+                );
+            }
+        })
+        var found = await firstLocatable(proxy);
+        assert.equal(found, "local-testing/test.js.map")
     })
 })
